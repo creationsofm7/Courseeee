2e8ecf2f7697f695538a35675c0b820437b93b8c
GHOST . <muditpanday17@gmail.com>
2024-05-01 12:20:42 +0530

    added github signin

--- src/app/auth.ts
+++ src/app/auth.ts
@@ -1,9 +1,9 @@
 import NextAuth from "next-auth"
 import { MongoDBAdapter } from "@auth/mongodb-adapter"
 import clientPromise from "./lib/db"
-import Resend from "next-auth/providers/resend"
+import Github from "next-auth/providers/github"
  
 export const { handlers, signIn, signOut, auth } = NextAuth({
   adapter: MongoDBAdapter(clientPromise),
-  providers: [Resend],
+  providers: [Github],
 })
--- src/app/components/sign-in.tsx
+++ src/app/components/sign-in.tsx
@@ -5,13 +5,12 @@ export function SignIn() {
     <form
       action={async (formData) => {
         "use server";
-        await signIn("resend", formData);
+        await signIn("github");
       }}
       className="flex flex-col items-center"
     >
-      <input type="text" name="email" placeholder="Email" className="border border-gray-300 rounded-md px-4 py-2 mb-2" />
       <button type="submit" className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
-        Signin with Resend
+        Signin with Github
       </button>
     </form>
   );
